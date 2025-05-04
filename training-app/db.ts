7212c4e18e79a114d4ba8cee92125ffc3d103402
xlporewebapp <xplorewebapp@gmail.com>
2025-05-04 18:25:36 -0400

    Delete training-app directory

--- training-app/db.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ellpsaghjktgjblpesqs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsbHBzYWdoamt0Z2pibHBlc3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MjMzNjksImV4cCI6MjA2MTE5OTM2OX0.0elJUnT0fNrhhEiyDyBKd6C2rwUAaL111ejo-LgExVk';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
